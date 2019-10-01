4df3a316c1ba08c0f285fc9d3416fd1355bac8ad
paulohcarellos <49373945+paulohcarellos@users.noreply.github.com>
2019-10-01 20:19:19 -0300

    Banco
    Banco implementado

--- Conta.h
+++ Conta.h
@@ -19,34 +19,38 @@ struct Data {
 	int ano;
 };
 
-class Conta {
-	
-	private :
+class Banco {
 
-		class Cliente {
+public:
 
-		private:
+	class Cliente {
 
-			string _Nome_cliente;
-			string _Cpf_cnpj;
-			string _Endereco;
-			string _Fone;
+	private:
 
-		public:
+		string _Nome_cliente;
+		string _Cpf_cnpj;
+		string _Endereco;
+		string _Fone;
 
-			Cliente();
-			Cliente(const Cliente &copia);
-			Cliente(string nome, string cpf, string endereco, string fone);
-			void Set_Nome(string nome);
-			void Set_Cpf_cnpj(string cpf);
-			void Set_Endereco(string endereco);
-			void Set_Fone(string fone);
-			string Get_nome();
-			string Get_Cpf_cnpj();
-			string Get_Endereco();
-			string Get_Fone();
+	public:
 
-		};
+		Cliente();
+		Cliente(const Cliente& copia);
+		Cliente(string nome, string cpf, string endereco, string fone);
+		void Set_Nome(string nome);
+		void Set_Cpf_cnpj(string cpf);
+		void Set_Endereco(string endereco);
+		void Set_Fone(string fone);
+		string Get_nome();
+		string Get_Cpf_cnpj();
+		string Get_Endereco();
+		string Get_Fone();
+
+	};
+
+	class Conta {
+
+	public:
 
 		class Movimentacao {
 
@@ -67,26 +71,52 @@ class Conta {
 			Data Get_Data();
 		};
 
-		int _Num_conta;
-		double _Saldo;
-		Cliente _Cliente;
-		vector<Movimentacao> _Movimentacoes;
-		static int _Proximo_num_conta;
-
-
-
-	public :
-
-		Conta();
-		Conta(double saldo, Cliente cliente, static int proxNumConta);
+		Conta(Banco::Cliente &cliente);
 		int Get_Numero_conta();
 		double Get_saldo();
-		string Get_cliente();	
+		Banco::Cliente Get_cliente();
 		bool Debitar(double, string); //Retorna true/false dependendo do sucesso da operacao
 		void Creditar(double, string);
 		vector<Movimentacao> extrato();
 		vector<Movimentacao> extrato(Data inicio);
 		vector<Movimentacao> extrato(Data inicio, Data fim);
+
+	private:
+
+		int _Num_conta;
+		double _Saldo;
+		Banco::Cliente _Cliente;
+		vector<Movimentacao> _Movimentacoes;
+		static int _Proximo_num_conta;
+
+	};
+
+	Banco(string nome);
+	void inserirCliente(Cliente &x);
+	void criarConta(Cliente &x);
+	void excluirCliente(string cpf);
+	void excluirConta(int nConta);
+	void deposito(int nConta, double valor);
+	void saque(int nConta, double valor);
+	void transferencia(int c1, int c2, double valor);
+	void tarifa();
+	void CPMF();
+	double saldo(int nConta);
+	vector<Conta::Movimentacao> extrato(int nConta);
+	vector<Conta::Movimentacao> extrato(int nConta, Data inicio);
+	vector<Conta::Movimentacao> extrato(int nConta, Data inicio, Data fim);
+	vector<Cliente> clientes();
+	vector<Conta> contas();
+	void save();
+	void load();
+
+
+private:
+
+	string _Banco;
+	vector<Cliente> _Clientes;
+	vector<Conta> _Contas;
+
 };
 
 #endif // CONTA_H
